c27bf596c92cf57f6fbb834aec2162233d080d04
Dean <dean@deankuhta.com>
2018-09-15 10:23:31 -0400

    playing with type conversion

--- ruby-work.rb
+++ ruby-work.rb
@@ -8,4 +8,11 @@ collie_hash = {"name"        => "collie",
                "lifespan"    => "13-16 years",
                "temperament" => "obedient"}
 
-puts "a collie's temperament is: " + collie_hash["temperament"] 
+puts "a collie's temperament is: " + collie_hash["temperament"] + "\n\n"
+
+#calculate the volume of a box
+length = 5
+width = 5
+height = 13
+volume = length * width * height
+puts "the volume of the box is: " + volume.to_s #convert int to string
