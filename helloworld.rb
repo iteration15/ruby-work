3393f50591243f857558c3b42a110dfaee2f1da7
Dean <dean@deankuhta.com>
2018-09-15 10:03:06 -0400

    created ruby-work.rb

--- helloworld.rb
+++ helloworld.rb
@@ -1,5 +1,15 @@
 puts "hello world!\n"
 
 foo = "foobar!"
+bar = 1
 
 puts foo
+puts bar
+
+light_on = false
+
+if (light_on)
+  puts foo
+else
+  puts "light is off!"
+end
---
+++ ruby-work.rb
@@ -0,0 +1,11 @@
+#playing with arrays
+dogs = ['collie','bulldog','beagle','chow']
+
+puts "the fourth dog in the list is: " + dogs[3] + "\n\n"
+
+#playing with hashes
+collie_hash = {"name"        => "collie",
+               "lifespan"    => "13-16 years",
+               "temperament" => "obedient"}
+
+puts "a collie's temperament is: " + collie_hash["temperament"] 
